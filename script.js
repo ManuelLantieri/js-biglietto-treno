2ce65314ce42dff16e28a802df30480472d41783
manuel lantieri <manuel.lantieri02@gmail.com>
2024-11-28 14:49:09 +0100

    prezzo scontato

--- script.js
+++ script.js
@@ -13,11 +13,19 @@ const chilometri = Number(prompt("Salve indicare il quantitativo di chilometri p
 const età = Number(prompt("Inserire l'età del passeggero"))
 //  Verifico il prezzo del biglietto
 //      -prezzo varia in base ai chilometri(motiplico il numero dei chilometri al prezzo/km)
-const prezzo = Number( chilometri * 0.21)
+const prezzo = Number(0.21 * chilometri)
 //  Verifico eventuali sconti
-//      -SE età è inferiore a 18(escluso) allora il prezzo è scontato del 20%
-if 
-//          oppure
-//      -SE età è superiore a 65(incluso) allora il prezzo è scontato del 40%
+//  sconto del 20%
+const scontoMinorenni = ((prezzo / 100) * 20 )
+const prezzoMinorenni = Number(prezzo - scontoMinorenni)
+//  sconto del 40%
+const scontoAnziani = ((prezzo / 100) * 40 )
+const prezzoAnziani = Number(prezzo - scontoAnziani)
+// SE età inferiore a 18(escluso) allora il prezzo sarà scontato del 20%
+if (età > 0 && età < 18) {
+    alert(`il prezzo del suo biglietto è di: ${prezzoMinorenni.toFixed(2)}€`)
+} else if (età >= 65 && età < 100) {
+    alert(`il prezzo del suo biglietto è di: ${prezzoAnziani.toFixed(2)}€`)
+}
 //      -ALTRIMENTI SE etàcompresa tra 18 (incluso) a 65(escluso) non viene applicato nessuno sconto
 //  prezzo finale da mostrare sarà espresso con massimo due decimali.
